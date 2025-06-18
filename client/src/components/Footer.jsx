f3a130faf3d041cc936d004756026d10399fcd29
Rishit Bansal <rishitbansal438@gmail.com>
2025-06-18 13:59:36 +0530

    Looks fine

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -4,11 +4,9 @@ const Footer = () => {
   return (
     <div className='container px-4 2xl:px-20 mx-auto flex items-center justify-between gap-4 py-3 mt-20'>
       <img width={160} src={assets.logo} alt="" />
-      <p className='flex-1 border-l border-gray-400 pl-4 text-sm text-gray-500 max-sm:hidden'>Copyright @Rishit Bansal.dev | All right reserved.</p>
+      <p className='flex-1 border-l border-gray-400 pl-4 text-sm text-gray-500 max-sm:hidden'>© 2025 Rishit Bansal. All rights reserved.</p>
       <div className='flex gap-2.5'>
-        <img width={38} src={assets.facebook_icon} alt="" />
-        <img width={38} src={assets.twitter_icon} alt="" />
-        <img width={38} src={assets.instagram_icon} alt="" />
+        <a href="https://github.com/RB133"><img width={38} src={assets.github_logo} alt="" /></a>
       </div>
     </div>
   )
